d5f74df3e3de1b620e1e5146c3eb5ac5e4e368f9
sam119 <samuel.pintodasilva@students.hevs.ch>
2019-10-07 09:38:52 +0200

    Logout correction

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -5,7 +5,7 @@ import NavLink from "reactstrap/lib/NavLink";
 export default function LogoutButton(){
     let usr = useAuth0();
     if(usr.isAuthenticated){
-        return <NavLink onClick={usr.logout} href="#">Logout</NavLink>
+        return <NavLink onClick={usr.logout} href="=#">Logout</NavLink>
     }
     return <NavLink></NavLink>
 }
