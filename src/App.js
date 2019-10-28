b648fcea7a6438e88b17d4b46fc000bb0078d8e2
Roger Schaer <roger.schaer@hevs.ch>
2019-10-28 16:26:35 +0100

    Router example

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { useAuth0 } from "./react-auth0-spa";
 import request from "./utils/request";
 import endpoints from "./endpoints";
 import Loading from "./components/Loading";
-import POI from "./components/POI";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import POIDetails from "./pages/POIDetails";
 
 function App() {
   let [pois, setPois] = useState([]);
@@ -33,18 +34,33 @@ function App() {
       <header className="App-header">
         <h1>Mapathon</h1>
         <br />
-        <a className="App-link" href="#" onClick={handlePOIsClick}>
-          Get POIs
-        </a>
-        {pois && pois.length > 0 && (
-          <ul className="POI-List">
-            {pois.map(poi => (
-              <li key={poi.id}>
-                <POI {...poi} />
-              </li>
-            ))}
-          </ul>
-        )}
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={() => (
+                <>
+                  <a className="App-link" href="#" onClick={handlePOIsClick}>
+                    Get POIs
+                  </a>
+                  {pois && pois.length > 0 && (
+                    <ul className="POI-List">
+                      {pois.map(poi => (
+                        <li key={poi.id}>
+                          <Link className="App-link" to={`/poi/${poi.id}`}>
+                            {poi.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </>
+              )}
+            />
+            <Route path="/poi/:id" component={POIDetails} />
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
   );
---
+++ src/pages/POIDetails.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import "./POIDetails.css";
+import POI from "../components/POI";
+import request from "../utils/request";
+import { useAuth0 } from "../react-auth0-spa";
+import endpoints from "../endpoints";
+import { Link } from "react-router-dom";
+
+export default function POIDetails({ match }) {
+  let poiID = +match.params.id;
+
+  let [poi, setPOI] = useState(null);
+
+  let { loading, loginWithRedirect, getTokenSilently } = useAuth0();
+
+  // Get POI details
+  useEffect(() => {
+    async function getPOI() {
+      let poi = await request(
+        `${process.env.REACT_APP_SERVER_URL}${endpoints.pois}/${poiID}`,
+        getTokenSilently,
+        loginWithRedirect
+      );
+
+      setPOI(poi);
+    }
+
+    getPOI();
+  });
+
+  return (
+    <div>
+      {poi ? <POI {...poi} /> : "Loading details..."}
+      <Link className="App-link" to="/">
+        Back
+      </Link>
+    </div>
+  );
+}
