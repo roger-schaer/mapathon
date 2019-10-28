9cc4dc58ac53cc8f4125dd22e93da0302012c052
viriot8u <viriotaudrey@gmail.com>
2019-10-28 15:37:07 +0100

    Change the details page in terms of editable, new or creator of the POI.

--- src/components/Details.js
+++ src/components/Details.js
@@ -15,6 +15,18 @@ export default function Details(props){
 
     let [poi, setPoi] = useState(0)
     let { loginWithRedirect, getTokenSilently } = useAuth0();
+    let [isLoaded, setIsLoaded] = useState(false);
+
+    let [isGoodUser, setIsGoodUser] = useState(false);
+
+    let currentUser = useAuth0().user;
+    let poiCreator = poi.Creator;
+    let [isEdit, setIsEdit] = useState(false);
+    let [valueButtonEdit, setValueButtonEdit] = useState("Edit")
+
+    let [newPOI, setNewPOI] = useState(null);
+    let [isNew, setIsNew] = useState(true);
+    let currentId = url.substring(url.lastIndexOf("/")+1);
 
     console.log(props.posClicked);
 
@@ -23,16 +35,70 @@ export default function Details(props){
             `${process.env.REACT_APP_SERVER_URL}${endpoints.pois}${'/'+param}`,
             getTokenSilently,
             loginWithRedirect,
+            setIsLoaded(true)
         ).then(token => {setPoi(token)} );
-        console.log(poi);
     }, []);
+
+    useEffect( () => {
+        console.log(currentId);
+        if(currentId === ""){
+            console.log("New POI");
+            setIsNew(true);
+            setNewPOI(defaultPOI);
+        }else if(isNaN(currentId)){
+            console.log("NaN is returned")
+            setIsNew(true);
+            setNewPOI(defaultPOI);
+        }else{
+            setIsNew(false);
+            setNewPOI(props.poi);
+            if(poi.error){
+                setIsNew(true);
+                setNewPOI(defaultPOI)
+            }else{
+                setNewPOI(poi);
+            }
+        }
+    }, [currentId, poi]);
+
+    let defaultPOI = {
+        name: '',
+        description:'',
+        lat:'',
+        lng:'',
+        image: '',
+        url:'',
+        group: 3,
+        Creator: null
+    }
+
+
+    let onClickEditButton = () => {
+        if(isEdit){
+            setIsEdit(false);
+            setValueButtonEdit("Edit")
+        }else{
+            setIsEdit(true);
+            setValueButtonEdit("Close edit mode")
+        }
+    }
 
         return(
             <div>
-                <POIForm thisPoi={poi}/>
+                {(poiCreator && currentUser.sub === poiCreator.id) &&
+                <div>
+                    <button onClick={onClickEditButton}>{valueButtonEdit}</button>
+                    <button>Delete</button>
+                </div>}
+                <POIForm thisPoi={poi} isEdit={isEdit} newPoi={newPOI} currentId={currentId} isNew={isNew}/>
                 <br/>
-                <BoxCategories/>
-                <BoxTags/>
+                {!isEdit && !isNew &&
+                <div>
+                    <BoxCategories/>
+                    <BoxTags/>
+                </div>}
+
+
             </div>
         );
 
--- src/components/POIForm.js
+++ src/components/POIForm.js
@@ -9,63 +9,29 @@ function POIForm(props){
 
     let { loginWithRedirect, getTokenSilently } = useAuth0();
     let url = window.location.href;
-    let [newPOI, setNewPOI] = useState(null);
-    let currentId = url.substring(url.lastIndexOf("/")+1);
-    let [isNew, setIsNew] = useState(true);
+
     let [isClicked, setIsClicked] = useState(false);
 
-    console.log(props.thisPoi.Creator)
-
-    let defaultPOI = {
-        name: '',
-        description:'',
-        lat:'',
-        lng:'',
-        image: '',
-        url:'',
-        group: 3,
-        Creator: null
-    }
-
-
-    useEffect( () => {
-        console.log(currentId);
-        if(currentId === ""){
-            console.log("New POI");
-            setIsNew(true);
-            setNewPOI(defaultPOI);
-        }else if(isNaN(currentId)){
-            console.log("NaN is returned")
-            setIsNew(true);
-            setNewPOI(defaultPOI);
-        }else{
-            setIsNew(false);
-            setNewPOI(props.thisPoi);
-            if(props.thisPoi.error){
-                setIsNew(true);
-                setNewPOI(defaultPOI)
-            }else{
-                setNewPOI(props.thisPoi);
-            }
-        }
-    }, [currentId, props.thisPoi]);
+    let currentUser = props.currentUser;
+    let poiCreator = props.thisPoi.Creator;
 
     return(
         <div className='detail-div'>
             <div className='img-div'>
                 <img style={{maxHeight: "100%", maxWidth: "100%", float: "right"}} src={props.thisPoi.image} alt="POI image"/><br/>
             </div>
+
             <div className='detail-content'>
                 <h1>{props.thisPoi.name}</h1>
-                {newPOI &&
+                {props.newPoi &&
                 <Formik
-                        initialValues={{    name: newPOI.name,
-                                            description: newPOI.description,
-                                            lat: newPOI.lat,
-                                            lng: newPOI.lng,
-                                            image: newPOI.image,
-                                            url: newPOI.url,
-                                            group: newPOI.group}}
+                        initialValues={{    name: props.newPoi.name,
+                                            description: props.newPoi.description,
+                                            lat: props.newPoi.lat,
+                                            lng: props.newPoi.lng,
+                                            image: props.newPoi.image,
+                                            url: props.newPoi.url,
+                                            group: props.newPoi.group}}
                     validate={values => {
                         let errors = {};
                         {/*Check required name*/}
@@ -79,7 +45,7 @@ function POIForm(props){
                             alert(JSON.stringify(values, null, 2));
                             setSubmitting(false);
 
-                            if(isNew){
+                            if(props.isNew){
                                 let response = await RequestPost(`${process.env.REACT_APP_SERVER_URL}${endpoints.pois}`,
                                     getTokenSilently,
                                     loginWithRedirect,
@@ -88,7 +54,7 @@ function POIForm(props){
                                 alert(response);
                                 console.log(response);
                                 console.log(response.id);
-                                currentId = response.id ;
+                                props.currentId = response.id ;
                             }else {
 
                             }
@@ -109,6 +75,7 @@ function POIForm(props){
                         <form onSubmit={handleSubmit}>
                             <span><h4>Latitude : </h4></span>
                             <input
+                                disabled={!(props.isEdit)}
                                 type="text"
                                 name="lat"
                                 onChange={handleChange}
@@ -117,6 +84,7 @@ function POIForm(props){
                             />
                             <span><h4>Longitude : </h4></span>
                             <input
+                                disabled={!(props.isEdit)}
                                 type="text"
                                 name="lng"
                                 onChange={handleChange}
@@ -125,7 +93,7 @@ function POIForm(props){
                             />
                             <span><h4>Name: </h4></span>
                             <input
-                                //disabled={props.isDisplayOnly} /*ReadOnly mod or not*/
+                                disabled={!(props.isEdit)} /*ReadOnly mod or not*/
                                 type="text"
                                 name="name"
                                 onChange={handleChange}
@@ -135,8 +103,7 @@ function POIForm(props){
                             {errors.name && touched.name && errors.name}
                             <span><h4>Description: </h4></span>
                             <textarea
-                                //disabled={props.isDisplayOnly}
-
+                                disabled={!(props.isEdit)}
                                 type="text"
                                 name="description"
                                 onChange={handleChange}
@@ -146,7 +113,7 @@ function POIForm(props){
                             {errors.description && touched.description && errors.description}
                             <span><h4>Website: </h4></span>
                             <input
-                                //disabled={props.isDisplayOnly}
+                                disabled={!(props.isEdit)}
                                 type="url"
                                 name="url"
                                 onChange={handleChange}
@@ -156,7 +123,7 @@ function POIForm(props){
                             {errors.url && touched.url && errors.url}
                             <span><h4>Image: </h4></span>
                             <input
-                                //disabled={props.isDisplayOnly}
+                                disabled={!(props.isEdit)}
                                 type="url"
                                 name="image"
                                 onChange={handleChange}
@@ -164,15 +131,19 @@ function POIForm(props){
                                 value={values.image}
                             />
                             {errors.image && touched.image && errors.image}
+                            {!props.isNew &&
                             <div>
-                                Created at <b>{props.thisPoi.createdAt}</b> by {props.thisPoi.Creator && <b>{props.thisPoi.Creator.name}</b>} (Group {props.thisPoi.group})
-                            </div>
-                            {true && (
+                                Created at <b>{props.thisPoi.createdAt}</b> by {props.thisPoi.Creator &&
+                            <b>{props.thisPoi.Creator.name}</b>} (Group {props.thisPoi.group})
                                 <div>Updated at <b>{props.thisPoi.updatedAt}</b></div>
-                            )}
-                            <button type="submit" disabled={isSubmitting}>
+                            </div>
+                            }
+                            {props.isEdit &&
+                                <button type="submit" disabled={isSubmitting}>
                                 Submit
-                            </button>
+                                </button>
+                            }
+
                         </form>
                     )}
                 </Formik>}
