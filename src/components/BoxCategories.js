5c62f9210190bcd31d11096b2b5b095e04905b99
viriot8u <viriotaudrey@gmail.com>
2019-10-31 17:20:25 +0100

    Categories correction 2.

--- src/components/BoxCategories.js
+++ src/components/BoxCategories.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ModalCategories from "./ModalCategories";
 import "./Box.css"
 import addLogo from "../assets/add-sign.png"
+import {Button, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
 
 export default function BoxCategories(props){
 
     let poiCategories = props.thisPoi.Categories;
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
 
     return(
         <div className="categories-box">
             <div><h3 style={{display: "inline-block"}}>Categories</h3>
                 <span> </span>{ poiCategories &&
-                <button className="button-add-category"><img style={{maxWidth: '15px'}} src={addLogo}/> Add</button>}
+                <button className="button-add-category" onClick={toggle}><img style={{maxWidth: '15px'}} src={addLogo}/> Add</button>}
             </div>
 
             {
@@ -23,6 +27,23 @@ export default function BoxCategories(props){
                     />
                 })
             }
+
+            <>
+                <Modal
+                    isOpen={modal}
+                    toggle={toggle}
+                >
+                    <ModalHeader toggle={toggle}>Audrey 1</ModalHeader>
+                    <ModalBody>
+                        Audrey 2<br/>
+                        <img style={{maxHeight: "30vh", maxWidth: "100%"}} src={props.imageCategorie} alt="POI image"/>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={toggle}>Audrey 3</Button>{' '}
+                        <Button color="secondary" onClick={toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </>
 
         </div>
 
